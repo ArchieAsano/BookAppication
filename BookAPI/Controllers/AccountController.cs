e1ec77e156d4851d34f6559ac43df700980353e7
Ly Hoang Khang <Khanglytvb@gmail.com>
2025-07-11 21:09:39 +0700

    fix(account): return JSON response in Register to support Android client

--- BookAPI/Controllers/AccountController.cs
+++ BookAPI/Controllers/AccountController.cs
@@ -2,7 +2,6 @@
 using DAL.DTO;
 using DAL.Library;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookAPI.Controllers
@@ -12,47 +11,83 @@ namespace BookAPI.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAccountService _accountService;
-        public AccountController(IAccountService accountService, ICartService cartService)
+
+        public AccountController(IAccountService accountService)
         {
             _accountService = accountService;
         }
+
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromForm] UserRegisterModel userRegisterModel)
         {
             try
             {
                 await _accountService.RegisterAccount(userRegisterModel);
-                
-                return Created();
+
+                return Ok(new
+                {
+                    success = true,
+                    message = "Đăng ký thành công!"
+                });
             }
             catch (Exception ex)
             {
-                return new BadRequestObjectResult(ex.Message);
+                return BadRequest(new
+                {
+                    success = false,
+                    message = "Đăng ký thất bại: " + ex.Message
+                });
             }
         }
+
         [HttpPost("Login")]
-        public async Task<IActionResult> Login (LoginRequestModel loginRequestModel)
+        public async Task<IActionResult> Login(LoginRequestModel loginRequestModel)
         {
             try
             {
                 var loginresponse = await _accountService.Login(loginRequestModel);
-                if (loginresponse == null) return new NotFoundObjectResult("Wrong Login Information");
-                return new OkObjectResult(loginresponse);
+                if (loginresponse == null)
+                {
+                    return NotFound(new
+                    {
+                        success = false,
+                        message = "Sai thông tin đăng nhập!"
+                    });
+                }
+
+                return Ok(new
+                {
+                    success = true,
+                    message = "Đăng nhập thành công!",
+                    data = loginresponse
+                });
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                return new BadRequestObjectResult(ex.Message);
+                return BadRequest(new
+                {
+                    success = false,
+                    message = "Đăng nhập thất bại: " + ex.Message
+                });
             }
         }
+
         [Authorize]
         [HttpGet("GetUserAccount")]
         public async Task<IActionResult> GetUserAccount()
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
+
             var email = User.GetUserEmail();
             var account = await _accountService.GetAccount(email);
-            return new OkObjectResult(account);
+
+            return Ok(new
+            {
+                success = true,
+                message = "Lấy thông tin người dùng thành công!",
+                data = account
+            });
         }
     }
 }
